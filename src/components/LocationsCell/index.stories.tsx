f603e3a629d8ef6bbb39ed6793d5272f8cbed134
AntenordosSantos <tthxcx@gmail.com>
2022-08-24 13:11:01 -0300

    cria LocationsCell

---
+++ src/components/LocationsCell/index.stories.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react-native';
+// import { action } from '@storybook/addon-actions';
+import styled from 'styled-components/native';
+import LocationsCell from './';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.color.gray.c25};
+  padding: 20px;
+`;
+
+export const actions = {
+  // onPress: action('onPress'),
+};
+storiesOf('components/LocationsCell', module).add('default', () => (
+  <Container>
+    <LocationsCell text={'LocationsCell Component'} {...actions} />
+  </Container>
+));
